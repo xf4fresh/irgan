0c85f84c28349de6c22765789d4375a153877325
xiaofei.ding <xiaofei.ding@qunar.com>
2017-11-24 19:56:08 +0800

    modeify dataset

--- ltr-gan/ltr-gan-pointwise/ltr_dns_nn.py
+++ ltr-gan/ltr-gan-pointwise/ltr_dns_nn.py
@@ -11,7 +11,7 @@ from dis_model_pairwise_nn import DIS
 
 FEATURE_SIZE = 46
 HIDDEN_SIZE = 46
-BATCH_SIZE = 32
+BATCH_SIZE = 8
 WEIGHT_DECAY = 0.01
 D_LEARNING_RATE = 0.0001
 
@@ -29,32 +29,40 @@ def create_dataset():
     query_pos_test = ut.get_query_pos(workdir + '/test.txt')
 
     # modify data
-    pos_feature = list()
+    pos_train = list()
+    pos_test = list()
     neg_feature = list()
     for query in query_url_feature.keys():
-
-        pos_url = list()
-        if query in query_pos_train:
-            pos_url.extend(query_pos_train[query])
+        pos_url_test = list()
+        pos_url_train = list()
         if query in query_pos_test:
-            pos_url.extend(query_pos_test[query])
+            pos_url_test = query_pos_test[query]
+        if query in query_pos_train:
+            pos_url_train = query_pos_train[query]
+
+        intersection_len = len(set(pos_url_test) & set(pos_url_train))
+        if intersection_len > 0:
+            print '[warn] query:%s, train&test count:%d' % (query, len(set(pos_url_test) & set(pos_url_train)))
 
         for url in query_url_feature[query].keys():
-            if url in pos_url:
-                pos_feature.append(query_url_feature[query][url])
+            if url in pos_url_train:
+                pos_train.append(query_url_feature[query][url])
+            elif url in pos_url_test:
+                pos_test.append(query_url_feature[query][url])
             else:
                 neg_feature.append(query_url_feature[query][url])
 
-    pos_arr = np.array(pos_feature, dtype='float32')
-    neg_arr = np.array(neg_feature, dtype='float32')
-    np.savez(dataset_path, pos_arr=pos_arr, neg_arr=neg_arr)
+    pos_train = np.array(pos_train, dtype='float32')
+    pos_test = np.array(pos_test, dtype='float32')
+    neg_all = np.array(neg_feature, dtype='float32')
+    np.savez(dataset_path, pos_train=pos_train, pos_test=pos_test, neg_all=neg_all)
 
 
 def generate_dns(sess, model, filename):
     data = []
     print('dynamic negative sampling ...')
 
-    pos_count = pos_data.shape[0]
+    pos_count = pos_train_data.shape[0]
     # neg_count = neg_data.shape[0]
 
     candidate_score = sess.run(model.pred_score, feed_dict={model.pred_data: neg_data})
@@ -71,7 +79,7 @@ def generate_dns(sess, model, filename):
     random.shuffle(data)
     with open(filename, 'w') as fout:
         for (pos, neg) in data:
-            fout.write(','.join([str(f) for f in pos_data[pos]]) + '\t'
+            fout.write(','.join([str(f) for f in pos_train_data[pos]]) + '\t'
                        + ','.join([str(f) for f in neg_data[neg]]) + '\n')
             fout.flush()
 
@@ -110,7 +118,7 @@ def main():
                                                              discriminator.neg_data: input_neg})
 
         p_5 = new_precision_at_k(sess, discriminator, test_features, test_labels, k=5)
-        ndcg_5 = new_ndcg_at_k(sess, discriminator, pos_test.shape[0], test_features, test_labels, k=5)
+        ndcg_5 = new_ndcg_at_k(sess, discriminator, pos_test_data.shape[0], test_features, test_labels, k=5)
 
         if p_5 > p_best_val:
             p_best_val = p_5
@@ -135,13 +143,13 @@ def main():
     p_5_best = new_precision_at_k(sess, discriminator_best, test_features, test_labels, k=5)
     p_10_best = new_precision_at_k(sess, discriminator_best, test_features, test_labels, k=10)
 
-    ndcg_1_best = new_ndcg_at_k(sess, discriminator_best, pos_test.shape[0], test_features, test_labels, k=1)
-    ndcg_3_best = new_ndcg_at_k(sess, discriminator_best, pos_test.shape[0], test_features, test_labels, k=3)
-    ndcg_5_best = new_ndcg_at_k(sess, discriminator_best, pos_test.shape[0], test_features, test_labels, k=5)
-    ndcg_10_best = new_ndcg_at_k(sess, discriminator_best, pos_test.shape[0], test_features, test_labels, k=10)
+    ndcg_1_best = new_ndcg_at_k(sess, discriminator_best, pos_test_data.shape[0], test_features, test_labels, k=1)
+    ndcg_3_best = new_ndcg_at_k(sess, discriminator_best, pos_test_data.shape[0], test_features, test_labels, k=3)
+    ndcg_5_best = new_ndcg_at_k(sess, discriminator_best, pos_test_data.shape[0], test_features, test_labels, k=5)
+    ndcg_10_best = new_ndcg_at_k(sess, discriminator_best, pos_test_data.shape[0], test_features, test_labels, k=10)
 
-    map_best = new_MAP(sess, discriminator_best, test_features, test_labels,)
-    mrr_best = new_MRR(sess, discriminator_best, test_features, test_labels,)
+    map_best = new_MAP(sess, discriminator_best, test_features, test_labels)
+    mrr_best = new_MRR(sess, discriminator_best, test_features, test_labels)
 
     print("Best ", "p@1 ", p_1_best, "p@3 ", p_3_best, "p@5 ", p_5_best, "p@10 ", p_10_best)
     print("Best ", "ndcg@1 ", ndcg_1_best, "ndcg@3 ", ndcg_3_best, "ndcg@5 ", ndcg_5_best, "p@10 ", ndcg_10_best)
@@ -156,15 +164,15 @@ if __name__ == '__main__':
 
     if True:
         npz_file = np.load(dataset_path)
-        pos_data, neg_data = npz_file['pos_arr'], npz_file['neg_arr']
-        # np.random.shuffle(neg_data)
-        neg_data = neg_data[:pos_data.shape[0] * 300]
-        print pos_data.shape, neg_data.shape
+        pos_train_data, pos_test_data, neg_data = npz_file['pos_train'], npz_file['pos_test'], npz_file['neg_all']
+        np.random.shuffle(neg_data)
+        print pos_train_data.shape, pos_test_data.shape, neg_data.shape
+        # exit(0)
 
-        from sklearn.model_selection import train_test_split
-        pos_train, pos_test = train_test_split(pos_data, random_state=2017, test_size=0.2)
-        neg_train, neg_test = train_test_split(neg_data, random_state=2017, test_size=0.2)
-        test_features = np.concatenate((pos_test, neg_test), axis=0)
-        test_labels = np.array([1] * pos_test.shape[0] + [0] * neg_test.shape[0])
+        # from sklearn.model_selection import train_test_split
+        # pos_train, pos_test = train_test_split(pos_data, random_state=2017, test_size=0.2)
+        # neg_train, neg_test = train_test_split(neg_data, random_state=2017, test_size=0.2)
+        test_features = np.concatenate((pos_test_data, neg_data), axis=0)
+        test_labels = np.array([1] * pos_test_data.shape[0] + [0] * neg_data.shape[0])
 
-    main()
+        main()
